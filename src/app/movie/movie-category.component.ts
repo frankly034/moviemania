20acb8d53a7db99edd848a3b28fedd368f8ba339
frankly034 <franklynugege@gmail.com>
2021-11-18 20:48:59 +0100

    feature(pagination): add pagination
    - add pagination params to movie-category-component
    - add pagination html to movie-category-html
    - add pagination style
    - modify movie-service

--- src/app/movie/movie-category.component.ts
+++ src/app/movie/movie-category.component.ts
@@ -19,6 +19,9 @@ export class MovieCategoryComponent implements OnInit, DoCheck, OnDestroy {
   private _filterBy: string = '';
   filteredMovies: Movie[] = [];
   movies!: Movie[];
+  page: number = 1;
+  totalPages: number = 0;
+  trigger: boolean = false;
 
   constructor(
     private route: ActivatedRoute,
@@ -26,14 +29,24 @@ export class MovieCategoryComponent implements OnInit, DoCheck, OnDestroy {
     private movieService: MovieService
   ) {}
 
+  nextPage(): void {
+    this.page = ++this.page;
+    this.trigger = true;
+  }
+
+  prevPage(): void {
+    this.page = --this.page;
+    this.trigger = true;
+  }
+
   moviesSub(category: string): void {
-    this.sub = this.movieService.getMovies(category).subscribe({
-      next: (movieResponse: MovieResponse) =>{
+    this.sub = this.movieService.getMovies(category, this.page).subscribe({
+      next: (movieResponse: MovieResponse) => {
         this.movies = movieResponse.results;
+        this.totalPages = movieResponse.total_pages;
         this.filteredMovies = this.movies;
       },
-      error: (err) =>
-        this.toastNotificationService.showError(err, 'Error'),
+      error: (err) => this.toastNotificationService.showError(err, 'Error'),
     });
   }
 
@@ -54,17 +67,18 @@ export class MovieCategoryComponent implements OnInit, DoCheck, OnDestroy {
   }
 
   ngOnInit(): void {
-    if(this.category){
-      this.moviesSub(this.category)
+    if (this.category) {
+      this.moviesSub(this.category);
     }
   }
 
   ngDoCheck(): void {
     const categoryParam = this.route.snapshot.paramMap.get('category');
-    if(categoryParam && this.category !== categoryParam){
-      this.moviesSub(categoryParam)
+    if (categoryParam && this.category !== categoryParam || categoryParam && this.trigger) {
+      this.moviesSub(categoryParam);
       this.category = categoryParam;
       this.title = this.route.snapshot.queryParamMap.get('title');
+      this.trigger = false;
     }
   }
 
