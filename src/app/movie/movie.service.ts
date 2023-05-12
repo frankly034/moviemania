c5698b288e39ad1093e4b301243c0848ab72f8e3
frankly034 <franklynugege@gmail.com>
2023-05-12 19:07:54 +0100

    Add environment variable

--- src/app/movie/movie.service.ts
+++ src/app/movie/movie.service.ts
@@ -1,33 +1,35 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators'
+import { catchError } from 'rxjs/operators';
 import { MovieResponse } from './movie-response';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MovieService {
-  apiKey = '523a1bbe12446728077d10c030a2d358'
+  apiKey = process.env.NG_APP_API_KEY;
+  movieUrl = process.env.NG_APP_TMDB_URL;
 
-  movieUrl = 'https://api.themoviedb.org/3/movie/';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  getMovies(movieCategoryTag: string, page?: number): Observable<MovieResponse> {
-    const url = `${this.movieUrl}${movieCategoryTag}?api_key=${this.apiKey}&page=${page || 1}`
-    return this.http.get<MovieResponse>(url).pipe(
-      catchError(this.handleError)
-    )
+  getMovies(
+    movieCategoryTag: string,
+    page?: number
+  ): Observable<MovieResponse> {
+    const url = `${this.movieUrl}${movieCategoryTag}?api_key=${
+      this.apiKey
+    }&page=${page || 1}`;
+    return this.http.get<MovieResponse>(url).pipe(catchError(this.handleError));
   }
 
-  handleError(err: HttpErrorResponse){
+  handleError(err: HttpErrorResponse) {
     let errorMessage = '';
-    if(err.error instanceof ErrorEvent){
+    if (err.error instanceof ErrorEvent) {
       errorMessage = `An error occured: ${err.error.message}`;
     } else {
       errorMessage = err.error.status_message || err.statusText;
     }
-    return throwError(errorMessage)
+    return throwError(errorMessage);
   }
 }
